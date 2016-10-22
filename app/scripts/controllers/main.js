aa45111141db3987f12868b670cfafbf054b047d
Julian Poyourow <julianpoyo@gmail.com>
2016-10-22 10:36:39 -0700

    Initialized the repo with Yeoman Angular!

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pollingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pollingApp
+ */
+angular.module('pollingApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
